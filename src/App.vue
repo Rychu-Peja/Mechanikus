4172ab0b9acbb8efb40063545c37063f210452da
Fryderyk Szmidt <f.szmidt.995@studms.ug.edu.pl>
2024-04-02 22:21:48 +0200

    add navbar and sidebar

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import NavBar from './components/NavBar.vue'
+import SideBar from './components/SideBar.vue'
+
+export default {
+  components: {
+    NavBar,
+    SideBar
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <NavBar />
+    <SideBar />
+    <main>
+     
+    </main>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
+
+
+
+
---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container"> 
+        <a class="navbar-brand" href="#">Mechanikus</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Rezerwacje</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Warsztaty</a>
+            </li>
+          </ul>
+        </div>
+        <div class="navbar-search"> 
+          <input type="text" class="form-control" placeholder="Wyszukaj">
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar'
+  }
+  </script>
+  
+  <style scoped>
+  .container {
+    padding-left: 15px; 
+    padding-right: 15px; 
+  }
+  
+  .navbar-search {
+    margin-left: auto; 
+    margin-right: auto; 
+  }
+  </style>
+  
